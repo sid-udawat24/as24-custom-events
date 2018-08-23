9fdc8142b5374185448326a5c3b254990c6ce454
Inaki Anduaga <inaki.anduaga@scout24.com>
2018-08-23 23:41:05 +0200

    Restructure folders, logic for how to easily define and consume types
    - Each page augments the DocumentEventMap namespace with the extra available events
    - addEventListeners automatically gain typechecks
    - customEvent creator needs a proxy method to enforce typechecks between name and payload

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as ListPage } from './pages/listpage'
+export { default as HomePage } from './pages/homepage'
+export { default as strictCustomEvent } from './constructors/strictCustomEvent'
