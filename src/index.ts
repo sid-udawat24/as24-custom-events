d9f5ca5eb7b87f0300bcd3817a6947e43b2033db
Inaki Anduaga <inaki.anduaga@scout24.com>
2018-08-27 14:53:17 +0200

    Add tests for main index exports

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@
 
 import * as ListPage from './pages/listpage'
 import * as HomePage from './pages/homepage'
+import * as Global from './pages/global'
 
 export { default as strictCustomEvent } from './constructors/strictCustomEvent'
 
 export { ListPage }
 export { HomePage }
+export { Global }
