62811db553f52c0b22c5f2c66fe16815a3dba8a6
Roman Timushev <roman.timushev@scout24.com>
2018-12-18 11:38:50 +0100

    Add watchlist readiness custom event

--- __tests__/pages/global.test.ts
+++ __tests__/pages/global.test.ts
@@ -1,17 +1,15 @@
 import * as p from '../../src/pages/global'
 
 describe('global events', () => {
-  const eventNames: string[] = []
+  const eventNames: string[] = [p.WatchlistEvent, p.FavoritesReady]
 
   test.each(eventNames)('event names must never change', eventName => {
     expect(eventName).toMatchSnapshot()
   })
 
   test('type definitions are inferred correctly by compiler', () => {
-    document.addEventListener(
-      p.WatchlistEvent,
-      e => (e.detail.type as 'WATCHLIST_COUNT_UPDATE') && (e.detail.value as number)
-    )
+    document.addEventListener(p.WatchlistEvent, e => e.detail.value as number)
+    document.addEventListener(p.FavoritesReady, e => e.detail as undefined)
     expect(true).toBeTruthy() // dummy assertion since compiler check is the real test
   })
 })
