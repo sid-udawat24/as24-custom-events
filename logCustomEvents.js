089f3c5c9f6c3732e6cae6317771be984f356558
Inaki Anduaga <inaki.anduaga@scout24.com>
2019-02-22 08:44:30 +0100

    Convert logCustomEvents function so it can run as a polyfill, include in bundle

--- logCustomEvents.js
+++ logCustomEvents.js
@@ -1,43 +1,50 @@
 /**
- * Paste this code on the browser console to see logs of all custom events fired on `document` root element
+ * Include this file into the dev build to see logs of all custom events fired on `document` root element
  */
-const standardEventNames = [
-  'DOMContentLoaded',
-  'MSGestureEnd',
-  'MSPointerCancel',
-  'MSPointerDown',
-  'MSPointerMove',
-  'MSPointerUp',
-  'animationend',
-  'blur',
-  'change',
-  'click',
-  'collapse',
-  'focus',
-  'input',
-  'keydown',
-  'keyup',
-  'mousedown',
-  'mouseover',
-  'pointercancel',
-  'pointerdown',
-  'pointermove',
-  'pointerup',
-  'scroll',
-  'selectionchange',
-  'touchcancel',
-  'touchend',
-  'touchmove',
-  'touchstart',
-  'transitionend',
-  'trckd',
-  'webkitAnimationEnd'
-]
+window.as24CustomEventsDebugListeners = (function() {
+  const standardEventNames = [
+    'DOMContentLoaded',
+    'MSGestureEnd',
+    'MSPointerCancel',
+    'MSPointerDown',
+    'MSPointerMove',
+    'MSPointerUp',
+    'animationend',
+    'blur',
+    'change',
+    'click',
+    'collapse',
+    'focus',
+    'input',
+    'keydown',
+    'keyup',
+    'mousedown',
+    'mouseover',
+    'pointercancel',
+    'pointerdown',
+    'pointermove',
+    'pointerup',
+    'scroll',
+    'selectionchange',
+    'touchcancel',
+    'touchend',
+    'touchmove',
+    'touchstart',
+    'transitionend',
+    'trckd',
+    'webkitAnimationEnd'
+  ]
 
-Object.keys(getEventListeners(document))
-  .filter(name => standardEventNames.indexOf(name) === -1)
-  .forEach(name =>
-    document.addEventListener(name, e =>
-      console.log(`%c Event ${name} pushed:`, 'color: green; font-weight:bold', e.detail)
-    )
-  )
+  // avoid double registration
+  if (window.as24CustomEventsDebugListeners !== true) {
+    Object.keys(getEventListeners(document))
+      .filter(name => standardEventNames.indexOf(name) === -1)
+      .forEach(name =>
+        document.addEventListener(name, e =>
+          console.info(`%c Event ${name}:`, 'color: green; font-weight:bold', e.detail)
+        )
+      )
+  }
+
+  return true
+})()
