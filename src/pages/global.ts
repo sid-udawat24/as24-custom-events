3a91e51433d5b08a276aba710ea3b243a90e0ced
Marco Bullin <marco.bullin@scout24.com>
2019-10-02 13:31:33 +0200

    Add custom notification subscription event

--- src/pages/global.ts
+++ src/pages/global.ts
@@ -20,10 +20,24 @@ export type WatchlistEvent = WatchlistEventCountUpdate
 export const FavoritesReady = 'FAVORITES:READY'
 export type FavoritesReady = undefined
 
+export const FavoriteAddedEvent = 'WATCHLIST:FAVORITE_ADDED'
+export const FavoriteRemovedEvent = 'WATCHLIST:FAVORITE_REMOVED'
+
+export type FavoriteAddedEventPayload = {
+  type: typeof FavoriteAddedEvent
+  value: string
+}
+export type FavoriteRemovedEventPayload = {
+  type: typeof FavoriteRemovedEvent
+  value: string
+}
+
 // inform compiler of the as24 custom events
 declare global {
   interface DocumentEventMap {
     [WatchlistEvent]: CustomEvent<WatchlistEvent>
     [FavoritesReady]: CustomEvent<FavoritesReady>
+    [FavoriteAddedEvent]: CustomEvent<FavoriteAddedEventPayload>
+    [FavoriteRemovedEvent]: CustomEvent<FavoriteRemovedEventPayload>
   }
 }
