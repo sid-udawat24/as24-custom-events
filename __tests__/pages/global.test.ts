d7f228dcb75875e22d9ec1d593a96e0c29e8f9f4
Marco Bullin <marco.bullin@scout24.com>
2020-07-28 14:59:36 +0200

    Add missing tests for new email subscription overlay event

--- __tests__/pages/global.test.ts
+++ __tests__/pages/global.test.ts
@@ -1,7 +1,13 @@
 import * as p from '../../src/pages/global'
 
 describe('global events', () => {
-  const eventNames: string[] = [p.WatchlistEvent, p.FavoritesReady, p.FavoriteAddedEvent, p.FavoriteRemovedEvent]
+  const eventNames: string[] = [
+    p.WatchlistEvent,
+    p.FavoritesReady,
+    p.FavoriteAddedEvent,
+    p.FavoriteRemovedEvent,
+    p.PcsUiShowOverlayEvent
+  ]
 
   test.each(eventNames)('event names must never change', eventName => {
     expect(eventName).toMatchSnapshot()
@@ -12,6 +18,7 @@ describe('global events', () => {
     document.addEventListener(p.FavoritesReady, e => e.detail as undefined)
     document.addEventListener(p.FavoriteAddedEvent, e => e.detail.value as string)
     document.addEventListener(p.FavoriteRemovedEvent, e => e.detail.value as string)
+    document.addEventListener(p.PcsUiShowOverlayEvent, e => e.detail.timeout as number)
     expect(true).toBeTruthy() // dummy assertion since compiler check is the real test
   })
 })
