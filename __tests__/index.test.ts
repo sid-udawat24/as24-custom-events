27ea0235dcaa1376344ca2c7a9d5a06c460e53c7
Marco Bullin <marco.bullin@autoscout24.com>
2020-12-10 11:30:40 +0100

    Fix some linting issues

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -3,6 +3,7 @@ import * as index from '../src/index'
 test('all expected exports are present', () => {
   expect(index.HomePage).not.toBeUndefined
   expect(index.ListPage).not.toBeUndefined
+  expect(index.LoginDialog).not.toBeUndefined
   expect(index.Global).not.toBeUndefined
   expect(index.strictCustomEvent).not.toBeUndefined
 })
