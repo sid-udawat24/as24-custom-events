54f338b7fd9d78f7a8643532c6b0876a10921993
Marco Bullin <marco.bullin@autoscout24.com>
2020-12-10 09:50:22 +0100

    Add AS24 Login Dialog Events

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import * as Global from './pages/global'
 import * as HomePage from './pages/homepage'
 import * as ListPage from './pages/listpage'
 import * as SavedSearchesEdit from './pages/savedSearchesEdit'
+import * as LoginDialog from './pages/loginDialog'
 
 export { default as strictCustomEvent } from './constructors/strictCustomEvent'
 
+export { LoginDialog }
 export { SavedSearchesEdit }
 export { ListPage }
 export { HomePage }
