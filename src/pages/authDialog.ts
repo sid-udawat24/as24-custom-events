1ee354e6225c90ed1ff5fa7ab69a0277f7c7f06c
Marco Bullin <marco.bullin@autoscout24.com>
2021-05-11 14:56:29 +0200

    Add additional parameter for redirect url

--- src/pages/authDialog.ts
+++ src/pages/authDialog.ts
@@ -2,12 +2,12 @@ export const AuthDialogTypeChangeEvent = 'AUTH_DIALOG_TYPE_CHANGE_EVENT'
 
 export enum AuthDialogType {
   SHOW_LOGIN_VIEW,
-  SHOW_REGISTRATION_VIEW,
   SHOW_SOCIAL_VIEW
 }
 
 export type AuthDialogTypeChange = {
   dialog: AuthDialogType
+  redirectUrl?: string
 }
 
 declare global {
