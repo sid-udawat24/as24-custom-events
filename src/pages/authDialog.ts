b08bc0bb416f9bef0d1a52b2f83593fcf519d025
Marco Bullin <marco.bullin@autoscout24.com>
2021-06-22 14:05:25 +0200

    Add override texts to the auth dialog event

--- src/pages/authDialog.ts
+++ src/pages/authDialog.ts
@@ -5,7 +5,6 @@ export type AuthDialogTypeChange = {
    * clientId - OKTA app id, see https://docs.services.as24.tech/autoscout24/products/identity/howto/#register-a-new-client
    */
   clientId: string
-
   /**
    * final redirect to go to after successful sign/signup. If not defined, the current window.location.href will be used.
    * IMPORTANT: If the value is not autoscout24 domain, no redirection will happens.
@@ -16,6 +15,25 @@ export type AuthDialogTypeChange = {
    * Used to track opening event in GA.
    */
   tp?: string
+  /**
+   * Override existing texts on social-, login and registration view within the overlay
+   */
+  overrideText?: {
+    // "Sign In"
+    signInTitle: string
+    // "Welcome!"
+    signInTeaserTitle: string
+    // "Log in and keep all your searches in one place."
+    signInTeaserDescription: string
+    // "Sign In"
+    socialTitle: string
+    // "Log in and keep all your searches in one place."
+    socialDescription: string
+    // "Sign up"
+    signUpTitle: string
+    // "Sign up and keep all your searches in one place."
+    signUpDescription: string
+  }
 }
 
 declare global {
